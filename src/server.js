4cdfdcaf61df92c19d69fdbe5c40e837c9457638
nemgu <xiufszh@gmail.com>
2022-12-27 12:22:37 +0900

    add Morgan Middleware

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  return res.send("I still love you.");
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hello");
 };
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
+
+const login = (req, res) => {
+  return res.send("login");
 };
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
