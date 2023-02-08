9cac73f0176dc087986a47503ae0c139b9e51b28
nemgu <xiufszh@gmail.com>
2023-02-08 16:10:02 +0900

    02/08 update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -65,6 +65,15 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
-  return res.render("search");
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
